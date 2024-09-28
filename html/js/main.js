d2ff11351525c23064f409f49d7286f3675b0948
soojihan <hdocarry@gmail.com>
2024-09-29 03:37:37 +0900

    fix : swiper-slide 위치 오류 발생, 수정 진행중

--- html/js/main.js
+++ html/js/main.js
@@ -1,5 +1,3 @@
-// 굿즈 마우스 오버 효과
-// 모든 .item 요소를 선택
 const items = document.querySelectorAll('.item');
 
 // 각 .item에 마우스 오버/아웃 이벤트 추가
@@ -7,14 +5,14 @@ items.forEach(item => {
     item.addEventListener('mouseenter', () => {
         const galleryDim = item.querySelector('.gallery-dim');
         if (galleryDim) {
-            galleryDim.classList.add('show'); // 마우스 올렸을 때 'show' 클래스 추가
+            galleryDim.classList.add('show');
         }
     });
 
     item.addEventListener('mouseleave', () => {
         const galleryDim = item.querySelector('.gallery-dim');
         if (galleryDim) {
-            galleryDim.classList.remove('show'); // 마우스 뗐을 때 'show' 클래스 제거
+            galleryDim.classList.remove('show');
         }
     });
 });
