c20ee7e6d26b952400d5d6172bc26393a80dcfb3
soojihan <hdocarry@gmail.com>
2024-09-30 23:06:47 +0900

    fix : 공통클래스 폰트 스타일 생성, header, footer 반응형 작업 진행중

--- html/js/main.js
+++ html/js/main.js
@@ -1,3 +1,13 @@
+
+$(function(){
+
+        // 네비 햄버거 버튼
+    $('#m-header-b .navBtn').on('click', function(){
+        $(this).toggleClass('slideOn');
+    });
+
+});
+
 const items = document.querySelectorAll('.item');
 
 // 각 .item에 마우스 오버/아웃 이벤트 추가
